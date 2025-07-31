a113bf8a27234519fbda50fc8e9b089ddc947ed7
chaithanya37 <chaithanyakanipakam37@gmail.com>
2025-07-31 10:30:01 +0530

    socket change

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-ba3m.onrender.com", {
+			const socket = io("https://mern-chaat-app.netlify.app/", {
 				query: {
 					userId: authUser._id,
 				},
